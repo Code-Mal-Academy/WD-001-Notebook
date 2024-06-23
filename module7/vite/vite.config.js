df8bf3077d0b985c79d4fc102d1c5e0be56f4ff0
Tom Everson <96673241+TomEverson@users.noreply.github.com>
2024-06-23 18:27:51 +0700

    Module 7

---
+++ module7/vite/vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, "index.html"),
+            },
+        },
+    },
+
+})
