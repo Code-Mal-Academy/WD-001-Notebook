955edeb65ec44f18abb890fdf0e8dd2d8795aa67
TomEverson <studycafe.125@gmail.com>
2024-08-11 20:44:01 +0700

    Mock Server Update

--- module4/mock-server/src/main.ts
+++ module4/mock-server/src/main.ts
@@ -1,9 +1,11 @@
 import express from "express"
+import cors from "cors"
 import data from "./db"
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", async (req, res) => res.status(200).send(data))
 
